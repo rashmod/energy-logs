e536e6e5a172bec67bab23527f8e2ae12208cd23
Bhushan Alhat <alhatbhushan@gmail.com>
2023-10-01 18:52:15 +0530

    [Frontend] Basic setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import ChartComponent from './components/ChartComponent';
+
+function App() {
+	return (
+		<div className='min-h-screen'>
+			<ChartComponent />
+		</div>
+	);
+}
+
+export default App;
---
+++ frontend/src/components/ChartComponent.tsx
@@ -0,0 +1,5 @@
+const ChartComponent = () => {
+	return <div>Chart</div>;
+};
+
+export default ChartComponent;
